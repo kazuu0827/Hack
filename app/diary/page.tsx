368e9da90a6e9fa28d7b7062d3bfc23595037946
kazuu0827 <kazuu0827@icloud.com>
2024-02-14 09:20:25 +0900

    Initial commit from Create Next App

---
+++ app/diary/page.tsx
@@ -0,0 +1,14 @@
+export default function Page() {
+  return (
+    // 中央揃えで表示
+    <div className="p-5 pb-32 flex flex-col items-center">
+      <h1 className="mb-10 text-cyan-600 text-3xl font-bold">
+        日記
+      </h1>
+
+      <p className="mb-10">
+        一日の記録を書きましょう
+      </p>
+    </div>
+  );
+}
